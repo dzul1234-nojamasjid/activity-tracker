40f7d36b541354d682557c4a863ca5d460662d54
dzul1234-nojamasjid <dzul1234@gmail.com>
2025-11-21 21:13:11 +0800

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,14 @@
+// Constants for your application
+const MISC_ITEMS_OPTIONS = [
+    "Mic",
+    "Flip Chart", 
+    "Projector",
+    "White Board",
+    "Marker",
+    "Laptop",
+    "Donation Box / Tabung",
+    "Carpark/Veh No.",
+    "Glass of Water", 
+    "File to Sign",
+    "On TV Screens"
+];
